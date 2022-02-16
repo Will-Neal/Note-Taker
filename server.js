6d12f9cf325dbf7ac4e9df849a56c1f14d7310be
Will Neal <willwneal@gmail.com>
2022-02-16 17:58:25 -0500

    created the paths described in the README

--- server.js
+++ server.js
@@ -1,17 +1,66 @@
-const express = require("express")
-const app = express()
-// const homepage = require("./public/index.html")
+const express = require("express");
+const fs = require("fs")
+const path = require("path");
+const app = express();
+//app.use(express.static(path.join(__dirname, 'public')));
+const db = require("./db/db.json");
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 const PORT = 3001;
 
-app.get('/', (req, res) => {
-    // console.info('get request is good')
-    res.send("THIS IS THE HOMEPAGE")
-})
+
+
+// sendFile will go here
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  });
+
+
 
 app.get('/api/notes', (req, res) => {
-    res.send("THIS IS THE NOTES PAGE")
+  res.json(db);
 })
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = Math.floor(Math.random() * 100000000);
+    console.log(newNote)
+    
+
+      // Obtain existing
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new note
+          parsedNotes.push(newNote);
+  
+          
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated database!')
+          );
+        }
+      });
+
+
+
+})
+
 
 
 app.listen(PORT, () =>
