92fa87285f19a7a438dce8b2e670a6c71d3f0a30
Will Neal <willwneal@gmail.com>
2022-02-17 19:21:06 -0500

    Added comments and cleaned up code

--- server.js
+++ server.js
@@ -3,30 +3,34 @@ const fs = require("fs")
 const path = require("path");
 const app = express();
 const db = require("./db/db.json");
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+//Port assignment that will default to 3001 unless assigned a port number by Heroku
 const PORT = 3001;
 
 
 
-// sendFile will go here
+//Sends the homepage 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
   });
 
+//Sends the notes page
 app.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
   });
 
 
-
+//Renders the notes from the JSON database
 app.get('/api/notes', (req, res) => {
   res.json(db);
 })
 
+//function that takes the note object and adds a unique id number to it using Math.Random and then pushes it to the database and the updated database to the JSON file. 
 function updateDatabase(note, database){
   const newNote = note;
   note.id = Math.floor(Math.random() * 1000)
@@ -39,6 +43,7 @@ function updateDatabase(note, database){
   );
 }
 
+//Deletes the chosen note. Gets id from the body parameters in the delete request and then uses a for loop to delete the object with the same id from the database and overwrites the database minus the deleted object. 
 function deleteNote(id, database){
   console.log("you hit the delete button")
   for (i=0; i<database.length; i++){
@@ -60,12 +65,14 @@ function deleteNote(id, database){
   }
 }
 
+//Delete request, sets noteId to the id extracted from req.params.id then uses that to call deleteNote function. Then sends to update the page.
 app.delete('/api/notes/:id', (req, res) => {
   noteId = req.params.id
   deleteNote(noteId, db)
   res.send()
 })
 
+//Post request, sets newNote = req.body to access properties set in index.js and then passes that and the database through to the updateDatabase function to add the new note to the database, then sends to update page. 
 app.post('/api/notes', (req, res) => {
   const newNote = req.body
   const renderNote = updateDatabase(newNote, db);
